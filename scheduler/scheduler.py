46a0537b48b88d9cf6c32546195e3b1b6a5f5392
MAK1z <66070055@kmitl.ac.th>
2025-09-20 09:56:55 +0700

    scheduler deploy

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -8,7 +8,7 @@ from database import get_router_info
 
 def scheduler():
 
-    INTERVAL = 30.0
+    INTERVAL = 1
     next_run = time.monotonic()
     count = 0
     host = os.getenv("RABBITMQ_HOST")
